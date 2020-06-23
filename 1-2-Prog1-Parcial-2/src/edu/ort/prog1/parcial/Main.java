25488503205ae9c7d3c3b29088b71cdab6e9358a
Matias Romera <romeramatias0@gmail.com>
2020-06-23 18:32:41 -0300

    Edit parcial

--- 1-2-Prog1-Parcial-2/src/edu/ort/prog1/parcial/Main.java
+++ 1-2-Prog1-Parcial-2/src/edu/ort/prog1/parcial/Main.java
@@ -9,24 +9,59 @@ public class Main {
 		// Instanciamos un garage
 		Garage garage = new Garage();
 			
-		// Estaciono 17 autos al garage, de tal manera que el primer piso
-		// quede lleno y pasemos a recorrer el segundo
-		System.out.println(garage.estacionar(new Vehiculo("FFF 111", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("GGG 222", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("TTT 333", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("RRR 444", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("EEE 555", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("SSS 666", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("YYY 777", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("MMM 888", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("QQQ 999", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("JJJ 110", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("OOO 120", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("DDD 130", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("HHH 140", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("III 150", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("NNN 160", "Ford", "F100")));
-		System.out.println(garage.estacionar(new Vehiculo("AAA 170", "Ford", "F100")));
+		// Estaciono 50 autos al garage, de tal manera que todos los pisos queden
+		// completamente llenos y al intentar estacionar uno mas, me de false
+		System.out.println(garage.estacionar(new Vehiculo("AAA 01", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 02", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 03", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 04", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 05", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 06", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 07", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 08", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 09", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 10", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 11", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 12", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 13", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 14", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 15", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 16", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 17", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 18", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 19", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 20", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 21", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 22", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 23", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 24", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 25", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 26", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 27", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 28", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 29", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 30", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 31", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 32", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 33", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 34", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 35", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 36", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 37", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 38", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 39", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 40", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 41", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 42", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 43", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 44", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 45", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 46", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 47", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 48", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 49", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA 50", "Ford", "F100")));
+		System.out.println(garage.estacionar(new Vehiculo("AAA EXCEDIDO", "Ford", "F100")));
 		System.out.println();
 		
 		// Muestro el garage con sus pisos y vehiculos
@@ -34,10 +69,11 @@ public class Main {
 		System.out.println();
 		
 		// Busco dos vehiculos por patente, uno valido y otro invalido
-		System.out.println(garage.retirar("GGG 222"));
+		System.out.println(garage.retirar("AAA 23"));
 		garage.retirar("sss");
 		System.out.println();
-		
+		System.out.println(garage.estacionar(new Vehiculo("AAA EXCEDIDO", "Ford", "F100")));
+
 		// Compruebo si el vehiculo GGG 222 fue retirado del piso
 		System.out.println(garage);
 		
